e01c589327125e5574efc701f841b11725be7349
Oluwaseun Akeredolu <oluseungideon@gmail.com>
2024-12-05 17:41:49 +0100

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,9 +5,13 @@ import { useAuth } from "../context/AuthContext";
 function Profile() {
   const { user } = useAuth(); // Fetch logged-in user details
   const [formData, setFormData] = useState({
-    name: user?.name || "",
-    email: user?.email || "",
-    password: user?.password || "",
+    firstName: user?.name?.split(" ")[0] || "",
+    lastName: user?.name?.split(" ")[1] || "",
+    phone: "",
+    matricNumber: "",
+    university: "",
+    department: "",
+    level: "",
   });
 
   const [avatar, setAvatar] = useState(null); // To handle avatar upload
@@ -42,22 +46,22 @@ function Profile() {
   return (
     <>
       <Header />
-      <div className="p-8 font-[Satoshi]">
-        <div className="mb-6 flex flex-col">
-          {/* Avatar */}
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex h-24 w-24 items-center justify-center rounded-full bg-blue-600 text-2xl font-bold text-white">
+      <div className="p-11 font-[Satoshi] bg-gray-100">
+        <div className="mb-16   flex flex-col justify-center items-center">
+          
+          <div className="flex flex-col  items-center">
+            <div className="flex h-24 w-24 items-center mb-8 justify-center rounded-full bg-blue-600 text-2xl font-bold text-white">
               {avatar ? (
                 <img
                   src={avatar}
                   alt="User Avatar"
-                  className="h-24 w-24 rounded-full object-cover"
+                  className="h-24 w-24  rounded-full object-cover"
                 />
               ) : (
                 `${user.name.split(" ")[0][0]}${user.name.split(" ")[1][0]}`
               )}
             </div>
-            {/* Change Picture Button */}
+            
             <div className="mt-2 flex space-x-4">
               <label className="cursor-pointer rounded bg-blue-500 px-4 py-2 text-white">
                 Change Picture
@@ -68,122 +72,110 @@ function Profile() {
                   onChange={handleAvatarUpload}
                 />
               </label>
-              <button
-                className="rounded bg-gray-400 px-4 py-2 text-white"
-                onClick={handleAvatarDelete}
-              >
-                Delete Image
-              </button>
+              {avatar && (
+                <button
+                  className="rounded bg-gray-400 px-4 py-2 text-white"
+                  onClick={handleAvatarDelete}
+                >
+                  Delete Image
+                </button>
+              )}
             </div>
           </div>
         </div>
 
-        {/* Editable Form */}
-        <form className="grid grid-cols-2 gap-4">
+        <form className="grid grid-cols-2 gap-11">
           <div>
-            <label className="block text-sm font-medium text-gray-600">
-              First name
+            <label className="block text-base mb-3 font-medium text-gray-600">
+              First Name
             </label>
             <input
               type="text"
-              name="name"
-              value={formData.name}
+              name="firstName"
+              value={formData.firstName}
               onChange={handleInputChange}
               className="w-full rounded-lg border border-gray-300 p-3"
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-600">
-              Last name
+            <label className="block text-base mb-3 font-medium text-gray-600">
+              Last Name
             </label>
             <input
               type="text"
-              name="name"
-              value={formData.name}
+              name="lastName"
+              value={formData.lastName}
               onChange={handleInputChange}
               className="w-full rounded-lg border border-gray-300 p-3"
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-600">
+            <label className="block text-base mb-3 font-medium text-gray-600">
               Phone Number
             </label>
             <input
               type="tel"
-              name="tel"
-              value={formData.email}
+              name="phone"
+              value={formData.phone}
               onChange={handleInputChange}
               className="w-full rounded-lg border border-gray-300 p-3"
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-600">
+            <label className="block text-base mb-3 font-medium text-gray-600">
               Matric Number
             </label>
             <input
-              type="number"
-              name="number"
-              value={formData.password}
+              type="text"
+              name="matricNumber"
+              value={formData.matricNumber}
               onChange={handleInputChange}
               className="w-full rounded-lg border border-gray-300 p-3"
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-600">
+            <label className="block text-base mb-3 font-medium text-gray-600">
               University
             </label>
             <input
               type="text"
-              name="password"
-              value={formData.password}
+              name="university"
+              value={formData.university}
               onChange={handleInputChange}
               className="w-full rounded-lg border border-gray-300 p-3"
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-600">
+            <label className="block text-base mb-3 font-medium text-gray-600">
               Department
             </label>
             <input
-              type="password"
-              name="password"
-              value={formData.password}
+              type="text"
+              name="department"
+              value={formData.department}
               onChange={handleInputChange}
               className="w-full rounded-lg border border-gray-300 p-3"
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-600">
-              Department
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
-              className="w-full rounded-lg border border-gray-300 p-3"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-600">
+            <label className="block text-base mb-3 font-medium text-gray-600">
               Level
             </label>
             <input
-              type="password"
-              name="password"
-              value={formData.password}
+              type="text"
+              name="level"
+              value={formData.level}
               onChange={handleInputChange}
               className="w-full rounded-lg border border-gray-300 p-3"
             />
           </div>
           <button
-            type="submit"
-            className="mt-4 rounded bg-blue-500 px-4 py-2 text-white"
+            type="button"
+            className="col-span-2 mt-4 w-full rounded bg-blue-500 px-4 py-2 text-white"
           >
             Save Changes
           </button>
         </form>
-        {/* </div> */}
       </div>
     </>
   );
