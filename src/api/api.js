ed830b0cb18df361a6a183b7b0db62174349aafa
Oluwapelumi <pelumi4069@gmail.com>
2025-03-01 10:28:08 +0100

    ....

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = "https://edstack-api.onrender.com/api/auth/";
-// const API_URL = "https://69fe-102-89-46-216.ngrok-free.app"
+
 
 const api = axios.create({
   baseURL: API_URL,
